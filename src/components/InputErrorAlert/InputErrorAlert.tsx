4b669fc7c5f1280d595a3faf7d6602932f9e3b69
hideyukisaito <_@hideyukisaito.info>
2022-05-18 07:10:57 +0900

    入力エラーの詳細を表示

--- src/components/InputErrorAlert/InputErrorAlert.tsx
+++ src/components/InputErrorAlert/InputErrorAlert.tsx
@@ -1,5 +1,7 @@
 import React from "react"
+import classNames from "classnames"
 import { useFormContext } from "react-hook-form"
+import { inputLabelsAndNames } from "../../constants/inputLabelsAndNames"
 
 type TProps = {
   isVisible?: boolean
@@ -9,38 +11,50 @@ const InputErrorAlert: React.FC<TProps> = ({ isVisible = false }) => {
   const { formState: { errors } } = useFormContext()
   const hasError = Object.keys(errors).length > 0
 
-  const errorTypes = Object.keys(errors).reduce((result: string[], key) => {
-    if (!result.includes(errors[key].type)) {
-      result.push(errors[key].type)
-    }
-    return result
-  }, [])
+  const errorMessages = Object.keys(inputLabelsAndNames)
+    .reduce((result: string[], key: string) => {
+        const name = inputLabelsAndNames[key].name
 
-  const classNames = [
-    'flex',
-    'flex-col',
-    'justify-center',
-    'items-center',
-    'gap-4',
-    'w-full',
-    'h-24',
-    'lg:h-32',
-    'text-sm',
-    'text-white',
-    'bg-error',
-  ]
+        if (errors.hasOwnProperty(inputLabelsAndNames[key].name)) {
+          let message = ''
 
-  classNames.push(hasError ? 'block' : 'hidden')
+          if (errors[name].type === 'required') {
+            message = `${inputLabelsAndNames[key].label}は必須項目です。`
+          } else if (errors[name].type === 'pattern') {
+            message = `${inputLabelsAndNames[key].label}の入力形式に誤りがあります。`
+          } else if (errors[name].type === 'validate') {
+            if (name === 'email-repeat') {
+              message = 'メールアドレスが一致していません。'
+            } else if (name === 'tel') {
+              message = '電話連絡をご希望の場合は電話番号を入力してください。'
+            }
+          }
+
+          result.push(message)
+        }
+        
+        return result
+    }, [])
 
   return (
-    <div className={classNames.join(' ')}>
-      <p className='font-bold lg:text-base'>入力内容に誤りがあります。</p>
-      {errorTypes.length > 0 &&
-        <ul className="flex flex-col text-xs lg:text-sm text-left gap-1">
-          {errorTypes.includes('required') && <li>・必須項目が入力されていません。</li>}
-          {errorTypes.includes('pattern') && <li>・入力形式に誤りがあります。</li>}
-        </ul>
-      }
+    <div className={classNames(
+      'flex', 'flex-col', 'justify-center', 'items-center', 'gap-4',
+      'w-full', 'h-auto',
+      'py-6',
+      'text-sm', 'text-white',
+      'bg-error',
+      hasError ? 'block' : 'hidden',
+    )}>
+      <p className='font-bold lg:text-base'>以下の入力内容をご確認ください。</p>
+      <ul className="flex flex-col text-xs lg:text-sm text-left gap-1">
+        {
+          Object.entries(errorMessages).reduce((result: JSX.Element[], item) => {
+            const [_, message] = item
+            result.push(<li>・{message}</li>)
+            return result
+          }, [])
+        }
+      </ul>
     </div>
   )
 }
