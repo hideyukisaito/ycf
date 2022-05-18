753c32cddb45a4e0a6dd08a739b64ae91a3ea479
hideyukisaito <_@hideyukisaito.info>
2022-05-18 11:40:55 +0900

    個人情報取り扱い同意のダミーリンクを設定

--- src/containers/ContactFormConfirmation/ContactFormConfirmation.tsx
+++ src/containers/ContactFormConfirmation/ContactFormConfirmation.tsx
@@ -255,7 +255,7 @@ export const ContactFormConfirmation: React.FC = () => {
             onChange={handleChangeConfirmationCheckbox}
           />
           <span className='ml-2 text-xs lg:text-base lg:pl-2'>
-            <a href='#_' className='text-blue-700 underline'>
+            <a href='https://www.google.com/' target='_blank' className='text-blue-700 underline'>
               <span>個人情報の取り扱いに関する同意事項</span>
               <svg xmlns="http://www.w3.org/2000/svg" className="inline h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
